f2d719be9142d036d408f5c00a989a02ab0a1656
alexbgh1 <alex.garnica@alumnos.uach.cl>
2024-04-22 21:19:03 -0400

    Fix error: delete build folder & updating package.json commands

--- build/constants/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_TEXT_COLOR = exports.DEFAULT_COLOR = exports.SIZE_LIMIT = void 0;
-const SIZE_LIMIT = 1000;
-exports.SIZE_LIMIT = SIZE_LIMIT;
-const DEFAULT_COLOR = "#eaeaea";
-exports.DEFAULT_COLOR = DEFAULT_COLOR;
-const DEFAULT_TEXT_COLOR = "#888";
-exports.DEFAULT_TEXT_COLOR = DEFAULT_TEXT_COLOR;
--- src/exception/badPathHandler.ts
+++ src/exception/badPathHandler.ts
@@ -5,7 +5,7 @@ function badPathHandler(req: Request, res: Response, next: NextFunction) {
     // Intentar decodificar el componente de la URL
     decodeURIComponent(req.path);
     next(); // Pasar al siguiente middleware si la decodificación fue exitosa
-  } catch (e) {
+  } catch (e: any) {
     // Si ocurre un error al decodificar, responder con un error 400
     if (e instanceof URIError) res.status(400).json({ error: `Bad request, path is not valid: '${req.url}'` });
     else res.status(500).json({ error: "Internal server error" });
